a6492732d9a4baae4c7930b7fe35fe82b1ed842a
AllHailMegatron <ahmadrthomp@gmail.com>
2019-05-10 19:27:47 -0400

    Created 3 objects in the objects-arrays.js file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,6 +13,29 @@
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
 // Using your dinosaur objects, log answers to these questions:
+let theropod = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous'
+}
+
+let stegos = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
+
+let velo = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+}
 
 // How much did tyrannosaurus weigh?
 console.log();
